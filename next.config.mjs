37b6f98d1a972906972d9e1c8d6973c89436aa12
mahmoudNEWdom <pprokiller2@gmail.com>
2025-04-14 21:18:58 +0200

    fix: Remove unused generateStaticParams function from PlaceDetails component feat: Enable dynamic routes in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: "standalone", // Add this line to enable dynamic routes
   compiler: {
     styledComponents: true,
     emotion: true,
